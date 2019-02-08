d0f8496ecc9faa3dd448ee5447cce792e5465d65
EC2 Default User <ec2-user@ip-10-0-0-76.ec2.internal>
2019-02-08 12:56:25 +0000

    comment

---
+++ main.tf
@@ -0,0 +1,25 @@
+#----------storage/main.tf-------
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a random id 
+
+resource "random_id" "tf_bucket_id" {
+  byte_length = 2
+  count = 2
+}
+
+# Create the bucket
+
+resource "aws_s3_bucket" "tf_code" {
+  count         = 2
+  bucket        = "${var.project_name}-${random_id.tf_bucket_id.*.dec[count.index]}"
+  acl           = "private"
+  force_destroy = true
+
+  tags {
+    Name = "tf_bucket_${count.index+1}"
+  }
+}
